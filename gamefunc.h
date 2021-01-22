fc672b9c4337df8414b171a434dc676c6604d714
fsdaf <fdsaf@a.pl>
2021-01-23 00:59:26 +0100

    malutki hot fiks

--- gamefunc.h
+++ gamefunc.h
@@ -5,13 +5,13 @@
 typedef struct figura
 {
     unsigned char fig;
-    int tim; //1 = biały, 2 = czarny
+    int tim; //1 = bia³y, 2 = czarny
 } figura;
 
 typedef struct savedMove
 {
     unsigned char fig;  // 'P', 'T', 'B', 'N', 'Q', 'K'
-    int tim;            // 1 = biały, 2 = czarny
+    int tim;            // 1 = bia³y, 2 = czarny
     int coords[4];      // yP,xP,yK,xK
     bool szach;         // czy w tym ruchu byl szach
 	bool mat;			// czy w tym ruchu byl mat
@@ -21,7 +21,7 @@ void initboard(figura board[8][8],int tryb);
 
 void printBoard(figura board[8][8],int tryb /*1==biale 0==czarne*/);
 
-bool isLegal(figura board[8][8],int yP,int x	P,int yK,int xK,int cas[4]);
+bool isLegal(figura board[8][8],int yP,int xP,int yK,int xK,int cas[4]);
 
 bool isChecked(int yKing, int xKing, figura board[8][8]);
 
